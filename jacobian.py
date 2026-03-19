871804536fdfcb2847dfa818bd8fcf85c44974c6
Michael Bliesath <mrb237@pitt.edu>
2026-03-19 09:43:27 -0400

    Milestone 7 - Not Done

--- jacobian.py
+++ jacobian.py
@@ -5,6 +5,25 @@ from circuit import Circuit
 
 class Jacobian:
     def __init__(self):
+        self.num_buses = len(self.buses)
 
+        # Count PV buses
+        self.num_pv = 0
+        for bus in self.buses.values():
+            if bus.bus_type == "PV":
+                self.num_pv += 1
+
+        self.size = (2 * self.num_buses) - 2 - self.num_pv
+
+        self.jacobian = np.zeros((self.size, self.size), dtype=float)
 
     def calc_jacobian(self, buses, ybus, angles, voltages):
+        for bus in self.buses.values():
+            if bus.bus_type == "PQ":
+                # P & Q
+
+            if bus.bus_type == "PV":
+                # P
+
+            if bus.bus_type == "Slack":
+                continue
