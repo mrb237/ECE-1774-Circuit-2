c3114123359d253b12b6027f84bfcfdec2575275
Michael Bliesath <mrb237@pitt.edu>
2026-03-25 13:27:40 -0400

    Updated through Milestone 8

--- bus.py
+++ bus.py
@@ -2,14 +2,14 @@ class Bus:
     index_counter = 0
     VALID_TYPES = {"PQ", "PV", "Slack"}
 
-    def __init__(self, name:str, nominal_kv:float):
+    def __init__(self, name:str, nominal_kv:float, bus_type:str = "PQ"):
         self.name = name
         self.nominal_kv = nominal_kv
         self.bus_index = Bus.index_counter
         Bus.index_counter += 1
         self.vpu = 1.0
         self.delta = 0.0
-        self._bus_type = "PQ"
+        self.bus_type = bus_type
 
     def __repr__(self):
         return f"Bus Nominal Voltage: {self.nominal_kv} Volts"
@@ -28,14 +28,14 @@ class Bus:
         self._bus_type = new_type
 
 if __name__ == "__main__":
-    bus1 = Bus("Bus1", 20.0)
+    bus1 = Bus("Bus1", 20.0, "Slack")
 
     print(f"Bus1 name: {bus1.name}")
     print(f"Bus1 Nominal Voltage: {bus1.nominal_kv} Volts")
     print(f"Bus1 Index: {bus1.bus_index}")
     print(f"Bus1 Type: {bus1.bus_type}")
 
-    bus2 = Bus("Bus2", 230.0)
+    bus2 = Bus("Bus2", 230.0, "PV")
 
     print(f"Bus2 name: {bus2.name}")
     print(f"Bus2 Nominal Voltage: {bus2.nominal_kv} Volts")
